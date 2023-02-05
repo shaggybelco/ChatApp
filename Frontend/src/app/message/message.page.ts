ae13b4089dfce8cec9e138c55f2b02361faa65a7
Shaggy <shaggybelco1@gmail.com>
2023-02-05 15:43:39 +0200

    all

--- Frontend/src/app/message/message.page.ts
+++ Frontend/src/app/message/message.page.ts
@@ -102,4 +102,24 @@ export class MessagePage implements OnInit {
       this.chat.sendMessage(messageData);
     }
   }
+
+  transform(date: any) {
+    if (!date) {
+      return 'a long time ago';
+    }
+    let time = (Date.now() - Date.parse(date)) / 1000;
+    if (time < 10) {
+      return 'just now';
+    } else if (time < 60) {
+      return 'a second ago';
+    }
+    const divider = [60, 60, 24, 30, 12];
+    const string = [' second', ' minute', ' hour', ' day', ' month', ' year'];
+    let i;
+    for (i = 0; Math.floor(time / divider[i]) > 0; i++) {
+      time /= divider[i];
+    }
+    const plural = Math.floor(time) > 1 ? 's' : '';
+    return Math.floor(time) + string[i] + plural + ' ago';
+  }
 }
